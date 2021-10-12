b10fae2c59b8f30f62e811aeffd20e315af70e58
Ritwik Rohitashwa <ritwik@kilobytetech.com>
2021-10-12 11:40:41 +0530

    [ADD] longest word checker

---
+++ Javascript/Maths/longestWord.js
@@ -0,0 +1,15 @@
+function findLongestWord(str) {
+  let words = str.split(" ");
+
+  let longest = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest) {
+      longest = words[i].length;
+    }
+  }
+
+  return longest;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
